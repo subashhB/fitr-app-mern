65be79c3a25f9144d60c53cfb9201bd8142b31e8
Admin <subashbaidya9@gmail.com>
2023-03-01 11:09:07 +0545

    Environment Variables

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 
 //Express App
 const app = express();
 
+//Routes
+app.get('/',(req,res)=>{
+     res.json({message:'Welcome to FitR'});
+})
+
 //Listening from Port 5000
-app.listen(5000, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log('Listening from Port 5000!');
 })
