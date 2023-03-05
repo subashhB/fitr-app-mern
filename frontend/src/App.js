7b38f24f61e4574b14a78148ef19731bcd978794
Admin <subashbaidya9@gmail.com>
2023-03-05 18:23:25 +0545

    Fetching Data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes,Route} from 'react-router-dom';
-import Navbar from './compoenents/Navbar';
+import Navbar from './components/Navbar';
 
 //Pages and Components
 import Home from './pages/Home';
