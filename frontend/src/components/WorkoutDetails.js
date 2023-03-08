f75d3e2cb6d8f4e84b8e5e0af55a5de9e6263f6a
Admin <subashbaidya9@gmail.com>
2023-03-08 09:59:44 +0545

    Delete Funtion

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,10 +1,24 @@
+import { useWorkoutContext } from '../hooks/useWorkoutsContext';
 const WorkoutDetails =({workout})=>{
+    
+    const { dispatch } = useWorkoutContext();
+    const handleDelete = async()=>{
+        const response = await fetch('/api/workouts/' +workout._id, {
+            method: 'DELETE'
+        })
+
+        const json = await response.json();
+        if(response.ok){
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        }
+    }
     return(
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load (in kg): </strong>{workout.load}</p>
             <p><strong>Repetition: </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleDelete}>Delete</span>
         </div>
     )
 }
