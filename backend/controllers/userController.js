65aff6a564e48b65216fa1adb29fa348b693f87d
Admin <subashbaidya9@gmail.com>
2023-03-10 11:56:56 +0545

    Signing Up & Hashing Passwords

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,14 @@ const loginUser = async(req,res)=>{
 
 //Sign Up User
 const signupUser = async(req,res) =>{
-    res.json({message: 'Sign-Up User'})
+    const {email,password} = req.body
+
+    try{
+        const user = await User.signup(email,password)
+        res.status(200).json({email,user})
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
 }
 
 module.exports = {loginUser, signupUser}
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema
 
@@ -13,5 +14,19 @@ const userSchema = Schema({
         required: true
     }
 })
+
+//Static SignUp method
+userSchema.statics.signup = async function(email, password){
+    const exists = await this.findOne({email})
+    if(exists){
+        throw Error('The E-mail already in use')
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({email, password: hash})
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
